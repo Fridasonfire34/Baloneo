96f487a298d678a44673b48d59cf8b6dee8fe1a7
FRIDA MICHELLE MUÑETON GUTIERREZ <frida.munetongtrz@uanl.edu.mx>
2024-09-28 08:02:52 -0600

    Resolved merge conflicts

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,12 @@ import path from 'path';
 const app = express();
 const port = 3000;
 
-// Servir archivos estáticos de la carpeta 'public'
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Ruta principal
 app.get('/', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// Iniciar el servidor
-app.listen(port, '0.0.0.0', () => {  // Cambiado a '0.0.0.0'
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running at http://0.0.0.0:${port}`);
 });
